26e675356232bc459fb6e332f9c8fe020b0f85d4
Krish <krishharsola717@gmail.com>
2026-01-18 20:26:32 +0530

    Time: 24 ms (77.41%) | Memory: 16.3 MB (68.07%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int rows, cols; // make them member variables
+
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == '0')
+            return;
+
+        // mark this cell as visited
+        grid[r][c] = '0';
+
+        // explore neighbors
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty()) return 0;
+
+        rows = grid.size();
+        cols = grid[0].size();
+        int count = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+};
