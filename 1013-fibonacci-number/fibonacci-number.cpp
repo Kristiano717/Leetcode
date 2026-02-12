519b4cc353ad16360b9d0789e56b720035066745
Krish <krishharsola717@gmail.com>
2026-02-12 16:33:35 +0530

    Time: 13 ms (31.73%) | Memory: 7.9 MB (36.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,8 +3,9 @@ public:
     int fib(int n) {
         if (n<=1)
         return n;
-        int last=fib(n-1);
-        int slast=fib(n-2);
+        int last =fib(n-1);
+        int slast= fib(n-2);
         return last + slast;
+
     }
 };
