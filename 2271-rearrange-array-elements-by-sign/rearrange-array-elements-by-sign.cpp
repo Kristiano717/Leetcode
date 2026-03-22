3d86ff78b0a288ddb39e41bcfacf49f058bda3ca
Krish <krishharsola717@gmail.com>
2026-03-22 19:06:06 +0530

    Time: 16 ms (9.62%) | Memory: 139.1 MB (18.47%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -6,7 +6,7 @@ public:
 
         int n = nums.size();
 
-        // Step 1: Separate
+        
         for (int i = 0; i < n; i++) {
             if (nums[i] < 0) {
                 neg.push_back(nums[i]);
@@ -15,7 +15,7 @@ public:
             }
         }
 
-        // Step 2: Merge alternately
+        
         int i = 0, p = 0, q = 0;
 
         while (p < pos.size() && q < neg.size()) {
@@ -23,10 +23,10 @@ public:
             nums[i++] = neg[q++];
         }
 
-        // Step 3: Remaining
+       
         while (p < pos.size()) nums[i++] = pos[p++];
         while (q < neg.size()) nums[i++] = neg[q++];
 
-        return nums; // ✅ inside function
+        return nums; 
     }
 };
