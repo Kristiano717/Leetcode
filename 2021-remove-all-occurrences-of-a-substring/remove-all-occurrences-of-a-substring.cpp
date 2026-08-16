e6bdf0ccd8a49be93529fb1ddbaea2b864164865
Krish <krishharsola717@gmail.com>
2026-08-16 09:10:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (72.26%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        //remove all the occureences in substring of noit single indivual chacraterf but  as word .
-        while (s.length()>0 &&  s.find(part)<s.size())
-        {
-            s.erase(s.find(part),part.size());
-        }
-        return s;
+      while(s.length()!=0 && s.find(part)<s.length()){
+       s.erase(s.find(part),part.length());
+      
+      }
+       return s;
     }
 };
