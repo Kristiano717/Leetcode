37410b814a589c4b6627ff0ed4fd3bd35152e202
Krish <krishharsola717@gmail.com>
2026-08-26 14:47:20 +0530

    Time: 378 ms (40.78%) | Memory: 311.9 MB (38.74%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -4,28 +4,29 @@ public:
         // our logic is that we should find ther path  if it exisst , the
         vector<vector<int>> adj(n);
         // destination if possible exists?
-        vector<int> visited(n, false);
-        stack<int> st;
+        vector<bool> visited(n, false);
+        queue<int> q;
         for (auto edge : edges) {
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
+         visited[source] =true; 
+            q.push(source);
+        
 
-        st.push(source);
-        while (!st.empty()) {
-            int node = st.top();
-            st.pop();
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
             if (!visited[node]) {
                 visited[node] = true;
             }
             if (node == dest) {
                 return true;
             }
-            for (int i = adj[node].size() - 1; i >= 0; i--) {
-                int nei = adj[node][i];
+            for (int nei : adj[node]) {
                 if (!visited[nei]) {
                     visited[nei] = true;
-                    st.push(nei);
+                    q.push(nei);
                 }
             }
         }
